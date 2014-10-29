ab23e79edc96cd6af04ac8164f1f734f6d5feeb0
dldemoks <demokstar@gmail.com>
2014-10-29 15:02:52 +0600

    update

--- Payeer (VirtueMart)/status.php
+++
@@ -1,8 +0,0 @@
-<?php
-$_REQUEST['option']='com_virtuemart';
-$_REQUEST['view']='pluginresponse';
-$_REQUEST['task']='pluginnotification';
-$_REQUEST['tmpl']='component';
-$_REQUEST['format'] = 'raw';
-$_REQUEST['pelement'] = 'payeer';
-include('../../../index.php');
